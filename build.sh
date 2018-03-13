86e5790ea938da59a6c99e57b31f558f07ec07df
Scott C. MacCallum <scm@linux.com>
2018-03-13 10:53:21 -0400

    Added file to build

--- build.sh
+++ build.sh
@@ -18,6 +18,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see http://www.gnu.org/licenses/.
 
-javac main.java meet.java
+javac main.java meet.java question.java
 
 exit 0
